27ba286bf0f4d270a93c9f6fdd31e15e778440d1
MLZ94 <zugutoroberto@gmail.com>
2021-05-27 23:01:58 +0200

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MLZ94/local_manifest --depth 1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+cd frameworks/nat* && git revert 883f4ec3a1b4bf64db2e761a7feffa946fb5f6df && git revert 73044d256a17ce52fa0b0bb18806d29cfe6911a1 && cd .. && cd ..
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch p404-X00QD-userdebug
+make p404
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/X00QD/*2021*.zip cirrus:X00QD -P
