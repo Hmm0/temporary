e7fe9a5f37b045ee10d985012eb7de31688b59ca
ECr34T1v3 <eriikgarciia1999@gmail.com>
2021-05-27 13:04:22 -0300

    beyond0lte: Build ArrowOS (test-01 - 27/04)

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ECr34T1v3/android_.repo_local_manifests.git --depth 1 -b beyond0-arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+. build/envsetup.sh
+lunch arrow_beyond0lte-userdebug
+m bacon
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/beyond0lte/*.zip cirrus:beyond0lte -P
