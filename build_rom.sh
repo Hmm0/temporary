23816049993d9d39d3f7f822c58d8a947951aade
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-28 08:39:20 +0530

    Zeus OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init -u https://github.com/Spark-Rom/manifest -b fire --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-ggit clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
+repo init -u https://github.com/ZenX-OS/android_manifest.git -b 11.1 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
-lunch spark_RMX1941-userdebug
-mka spark
+lunch zeus_RMX1941-userdebug
+brunch RMX1941
 
 
 # upload
